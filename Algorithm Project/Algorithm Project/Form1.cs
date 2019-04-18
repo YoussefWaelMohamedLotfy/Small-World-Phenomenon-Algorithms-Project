3d64b4d5c8d42865651f7c5674cc1c7fad44bd89
magdy yakob <magdyyakob323@outlook.com>
2019-04-18 11:55:04 +0200

    c

--- Algorithm Project/Algorithm Project/Form1.cs
+++ Algorithm Project/Algorithm Project/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Algorithm_Project
 {
@@ -20,10 +21,37 @@ namespace Algorithm_Project
         }
 
         #region Test Case Choice
+        List<Node> my_data = new List<Node>();
         private void sampleTest_btn_Click(object sender, EventArgs e)
         {
             testCase = "Sample";
             selectedTestCaseLabel.Text = "Selected Test Case : " + testCase;
+            try
+            {
+                //read the "movie1" file from (Sample Test Cases)
+                FileStream fs = new FileStream("movies1.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                while (sr.Peek() != -1)
+                {
+                    string record;
+                    record = sr.ReadLine();
+                    string[] recordContent = record.Split('/');
+                    Node temp = new Node();
+                    temp.movie = recordContent[0];
+                    for (int i = 1; i < recordContent.Length; i++)
+                    {
+                        temp.actors.Add(recordContent[i]);
+                    }
+                    my_data.Add(temp);
+                    //record = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
+
+            }
         }
 
         private void smallTest_btn_Click(object sender, EventArgs e)
---
+++ Algorithm Project/Algorithm Project/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Project
+{
+    class Node
+    {
+        public string movie;
+        public List<string> actors = new List<string>();
+    }
+}
